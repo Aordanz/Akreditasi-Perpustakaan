fe0631d488208bfc69ac655d888006d827b336f6
Jojooo4 <joybarus447@gmail.com>
2026-07-30 16:23:25 +0700

    try

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', __('Perpustakaan USU'))</title>
+    <link rel="icon" href="{{ asset('logousu.jpeg') }}" type="image/jpeg">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     <!-- Alpine.js -->
